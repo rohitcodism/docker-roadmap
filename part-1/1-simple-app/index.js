f31f968e9267b79080d804d8c948a466e1368224
rohitcodism <rohitpaula18i@gmail.com>
2023-10-28 09:37:17 +0530

    Docker basics check

--- part-1/1-simple-app/index.js
+++ part-1/1-simple-app/index.js
@@ -1,8 +1,6 @@
-
-
 const express = require('express')
 const app = express()
-const port = 5500
+const port = 3000
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
@@ -16,3 +14,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+console.log(__dirname);
